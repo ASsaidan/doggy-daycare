9818952df36e1b6fd283e76185bb3e12cd8ceb1a
Saidan <hh@Hs-MacBook-Air.lan>
2024-10-06 17:27:19 +0200

    Updates

---
+++ precache-manifest.e93c64c96120cc481c6a075cd742fc5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140a090c8d0b7df0b16749450c39618e",
+    "url": "/doggy-daycare/index.html"
+  },
+  {
+    "revision": "a36dfdf8b348216c0f95",
+    "url": "/doggy-daycare/static/css/main.aac381a1.chunk.css"
+  },
+  {
+    "revision": "60cc7c3b4e678e78af7d",
+    "url": "/doggy-daycare/static/js/2.6af88a27.chunk.js"
+  },
+  {
+    "revision": "afae8725e7d5172c6ee86f29e0116382",
+    "url": "/doggy-daycare/static/js/2.6af88a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d020372ded26d32d22b",
+    "url": "/doggy-daycare/static/js/3.da762b4d.chunk.js"
+  },
+  {
+    "revision": "a36dfdf8b348216c0f95",
+    "url": "/doggy-daycare/static/js/main.e7036783.chunk.js"
+  },
+  {
+    "revision": "6abbb3da36a176c6cee2",
+    "url": "/doggy-daycare/static/js/runtime-main.3955b891.js"
+  }
+]);
