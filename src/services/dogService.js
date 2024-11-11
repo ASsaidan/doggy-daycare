9036b3dd821623e1ca9e6075b1a3b638be6972a7
Saidan <Ahmad.emos1@gmail.com>
2024-11-11 11:46:43 +0100

    new dog jsonbin api added  and website should be working

--- src/services/dogService.js
+++ src/services/dogService.js
@@ -1,13 +1,32 @@
+// src/services/dogService.js
 import axios from 'axios';
 
-const API_URL = 'https://api.jsonbin.io/v3/b/66ea6857e41b4d34e4325758';
+const API_URL = 'https://api.jsonbin.io/v3/b/6731d700acd3cb34a8a66a4e';
+const MASTER_KEY = "$2a$10$Ak8PmSt3H8tmQUhk2n5gs.yhOd9c9gXRax30RRTwuw6KnWJH.Lke2";
 
 export const fetchDogs = async () => {
   try {
-    const response = await axios.get(API_URL);
-    return response.data.record;
+    const response = await axios.get(API_URL, {
+      headers: {
+        'X-Master-Key': MASTER_KEY,
+        'Content-Type': 'application/json',
+        'X-Bin-Meta': false
+      }
+    });
+
+    if (!response.data.record.dogs) {
+      console.error('No dogs data found in response');
+      return [];
+    }
+
+    return response.data.record.dogs;
+
   } catch (error) {
     console.error('Error fetching dogs:', error);
+    if (error.response) {
+      console.log('Error details:', error.response.data);
+      console.log('Error status:', error.response.status);
+    }
     return [];
   }
 };
